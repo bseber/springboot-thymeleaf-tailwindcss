fc6a3e2d8d654ff479f8ea6cc6586c0067623e27
Benjamin Seber <seber@synyx.de>
2019-10-01 15:21:48 +0200

    use indent_size 2 for java files
    I just like it more ¯\_(ツ)_/¯

--- src/main/java/com/github/bseber/SpringBootThymeleafTailwindCSSApplication.java
+++ src/main/java/com/github/bseber/SpringBootThymeleafTailwindCSSApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBootThymeleafTailwindCSSApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(SpringBootThymeleafTailwindCSSApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootThymeleafTailwindCSSApplication.class, args);
+	}
 }
--- src/main/java/com/github/bseber/web/WebSecurityConfig.java
+++ src/main/java/com/github/bseber/web/WebSecurityConfig.java
@@ -13,6 +13,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http
 			.authorizeRequests()
-				.anyRequest().permitAll();
+			.anyRequest().permitAll();
 	}
 }
